5d6fc258aeec5b9aac6507600b83b1bf684ed553
Avinash Dwivedi <141247046+AvinashD1010@users.noreply.github.com>
2023-12-12 16:05:32 +0530

    chime: PixelOS

--- custom.mk
+++ custom.mk
@@ -11,13 +11,13 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Live Wallpaper
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
+# Face unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Google Assistant (New Generation)
 PRODUCT_PACKAGES += \
     NgaResources \
     nga
 
-# Recovery
-TARGET_USES_AOSP_RECOVERY := true
+# Live Wallpaper
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
